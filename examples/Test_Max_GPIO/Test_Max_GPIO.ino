951d5909b9449bf6b1e77bd21212c1f3511e3ba9
Sudar <sudar@sudarmuthu.com>
2012-10-29 21:07:01 +0530

    Added an example sketch

---
+++ examples/Test_Max_GPIO/Test_Max_GPIO.ino
@@ -0,0 +1,37 @@
+#include <USB_Host_Shield_GPIO.h>
+
+MaxGPIO max;
+
+void setup()
+{
+    Serial.begin(9600);
+}
+
+void loop()
+{
+    max.write(0, HIGH);
+    max.write(1, HIGH);
+    max.write(2, HIGH);
+    max.write(3, HIGH);
+    max.write(4, LOW);
+    max.write(5, LOW);
+    max.write(6, LOW);
+    max.write(7, LOW);
+    Serial.print("0: "); Serial.println(max.read(0));
+    Serial.print("1: "); Serial.println(max.read(1));
+
+    delay(1000);
+
+    max.write(0, LOW);
+    max.write(1, LOW);
+    max.write(2, LOW);
+    max.write(3, LOW);
+    max.write(4, HIGH);
+    max.write(5, HIGH);
+    max.write(6, HIGH);
+    max.write(7, HIGH);
+    Serial.print("0: "); Serial.println(max.read(0));
+    Serial.print("1: "); Serial.println(max.read(1));
+
+    delay(1000);
+}
